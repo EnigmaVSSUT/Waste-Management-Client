ea78649f50ecb871bbfaa588c680e8f5305073f8
Ravindra Nag <ravindranag52@gmail.com>
2022-11-01 22:14:30 +0530

    feat: added fonts

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import { Button, Stack } from "@mui/material";
+
+const Index = () => {
+	return (
+		<Stack
+			padding='64px 32px'
+			gap='32px'
+		>
+
+		</Stack>
+	);
+}
+
+export default Index;
--- theme/options/typography.js
+++ theme/options/typography.js
@@ -1,16 +1,18 @@
-import '@fontsource/plus-jakarta-sans/400.css'
-import '@fontsource/plus-jakarta-sans/500.css'
-import '@fontsource/plus-jakarta-sans/600.css'
+// import '@fontsource/plus-jakarta-sans/400.css'
+// import '@fontsource/plus-jakarta-sans/500.css'
+// import '@fontsource/plus-jakarta-sans/600.css'
 
 
 const typography = {
-	fontFamily: 'Plus Jakarta Sans',
+	fontFamily: ['Product Sans', 'sans-serif'].join(','),
 	'headline.small': {
+		fontFamily: 'Product Sans',
 		fontWeight: 400,
 		fontSize: '24px',
 		lineHeight: '32px'
 	},
 	'title.small': {
+		fontFamily: 'Product Sans Bold',
 		fontWeight: 700,
 		fontSize: '14px',
 		lineHeight: '20px'
